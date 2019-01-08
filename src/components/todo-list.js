ce611793cffd3b9bde2165fb3e0ed00b79185a9a
SkutinAnton <Skutin_anton@mail.ru>
2019-01-08 17:39:02 +0300

    feat: add todo list and todo item

---
+++ src/components/todo-list.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ToDoListItem from './todo-list-item';
+
+const ToDoList = ({ todos }) => {
+
+  const elements = todos.map((item) => {
+    return (
+      <li>
+        <ToDoListItem {...item} />
+      </li>
+    );
+  });
+
+  return (
+    <ul>
+      { elements }
+    </ul>
+  );
+};
+
+export default ToDoList;
