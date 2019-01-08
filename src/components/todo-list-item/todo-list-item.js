f361518e5f5e206070a05ea5051c3652f0c307e7
SkutinAnton <Skutin_anton@mail.ru>
2019-01-08 21:17:20 +0300

    feat: add class to project

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const ToDoListItem = ({ label, important }) => {
-  const style = {
-    color: important ? 'lightslategrey' : 'black',
-  };
+export default class ToDoListItem extends Component {
 
-  return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>
-        {label}
+  onLabelClick = () => {
+    console.log(this.props.label);
+  }
+
+  render() {
+    const { label, important } = this.props;
+
+    const style = {
+      color: important ? 'lightslategrey' : 'black',
+    };
+  
+    return (
+      <span className="todo-list-item">
+        <span className="todo-list-item-label" style={style} onClick={this.onLabelClick}>
+          {label}
+        </span>
+  
+        <button type="button" className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+  
+        <button type="button" className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation" />
+        </button>
       </span>
-
-      <button type="button" className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-
-      <button type="button" className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-    </span>
-  );
-};
-
-export default ToDoListItem;
+    );
+  }
+}
