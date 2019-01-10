71547f6ffcdc69b6ffdbd0b4febb6fe03447c805
SkutinAnton <Skutin_anton@mail.ru>
2019-01-10 23:21:38 +0300

    feat: add form event

--- src/components/add-item-form/add-item-form.js
+++ src/components/add-item-form/add-item-form.js
@@ -4,13 +4,29 @@ import './add-item-form.css';
 
 export default class AddItemForm extends Component {
 
+  state = {
+    label: ''
+  }
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  }
+
   render() {
     return (
-      <div className="add-item-form">
-        <button className="btn btn-outline-secondary" onClick={() => this.props.onItemAdded('Hello')}>
+      <form className="add-item-form" onSubmit={this.onSubmit}>
+        <input type="text" className="form-control" onChange={this.onLabelChange} placeholder="What need to be done"/>
+        <button className="btn btn-outline-secondary">
           Add Item
         </button>
-      </div>
+      </form>
     )
   };
 };
