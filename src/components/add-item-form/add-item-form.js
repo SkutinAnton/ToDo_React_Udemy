cff0a8d757e0099d1bb60fc482580bb6b35a0de2
SkutinAnton <Skutin_anton@mail.ru>
2019-01-10 23:28:16 +0300

    feat: clear form after add new todo

--- src/components/add-item-form/add-item-form.js
+++ src/components/add-item-form/add-item-form.js
@@ -17,12 +17,22 @@ export default class AddItemForm extends Component {
   onSubmit = (e) => {
     e.preventDefault();
     this.props.onItemAdded(this.state.label);
+
+    this.setState({
+      label: ''
+    })
   }
 
   render() {
     return (
       <form className="add-item-form" onSubmit={this.onSubmit}>
-        <input type="text" className="form-control" onChange={this.onLabelChange} placeholder="What need to be done"/>
+        <input 
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What need to be done"
+          value={this.state.label}
+        />
         <button className="btn btn-outline-secondary">
           Add Item
         </button>
